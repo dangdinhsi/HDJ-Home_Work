149438afe63cbbe824850f9a1a990605f1e30eb5
dangdinhsi <siddd00474@fpt.edu.vn>
2018-10-15 09:05:13 +0700

    upload file homework

---
+++ Hdj_Homework/Data/Hdj_HomeworkContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hdj_Homework.Models
+{
+    public class Hdj_HomeworkContext : DbContext
+    {
+        public Hdj_HomeworkContext (DbContextOptions<Hdj_HomeworkContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Hdj_Homework.Models.Member> Member { get; set; }
+    }
+}
---
+++ Hdj_Homework/Models/Member.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hdj_Homework.Models
+{
+    public class Member
+    {
+        private int id;
+        private string name;
+        private string email;
+        private string phone;
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public string Email { get => email; set => email = value; }
+        public string Phone { get => phone; set => phone = value; }
+    }
+}
